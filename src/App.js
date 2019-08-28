b8a58729369d2854e173c5c0c91086c4112cac9b
yykdev <assavictory@gmail.com>
2019-08-28 17:49:01 +0900

    react와 jsx 의 기본

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Potato from "./potato";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <Potato></Potato>
+        </div>
+    );
 }
 
 export default App;
