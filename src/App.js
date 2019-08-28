b5dcc6a3ce3cca9fdf7518e329ee59a78d31d39a
yykdev <assavictory@gmail.com>
2019-08-28 23:25:10 +0900

    샘플 데이터를 통한 리스트 (반복) 렌더링 = map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React from "react";
 
-function Food({ favourite }) {
-    console.log(favourite);
-    return <h1>I Love {favourite}</h1>;
+function Food({ name, picture }) {
+    return (
+        <div>
+            <h1>I Love {name}</h1>
+            <img src={picture} alt="" />
+        </div>
+    );
 }
+
+const foodILike = [
+    {
+        name: "kimchi",
+        image:
+            "http://blogfiles.naver.net/20150603_66/harmony2154_14333061055283Pyea_JPEG/%B4%D9%B1%E8%C4%A13.jpg"
+    },
+    {
+        name: "ramen",
+        image:
+            "http://blogfiles.naver.net/20131107_4/mkpark_88_1383797435291DSieE_JPEG/%B6%F3%B8%E9%C3%E0%C1%A62.jpg"
+    },
+    {
+        name: "samgipsal",
+        image:
+            "http://blogfiles.naver.net/MjAxNzA3MTFfMjMy/MDAxNDk5NzUxNDgwMTc3._0Mko9MUsaQzQHRTv4TJP2EB1Xljmv1hjKNGtk6kj54g.AdARpLw4YkXYT-c3LbXBiXBcCIyZ1doCs3Ur_M7YmnAg.JPEG.sisaic/%BB%EF%B0%E3%BB%EC.jpg"
+    },
+    {
+        name: "chukumii",
+        image:
+            "http://blogfiles.naver.net/20150529_213/jhkt114_1432877185374YAVYE_JPEG/%C2%DE%BA%D23.jpg"
+    }
+];
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello</h1>
-            <Food favourite="kimchi" />
-            <Food favourite="ramen" />
-            <Food favourite="samgipsal" />
-            <Food favourite="chukumii" />
+            {foodILike.map(dish => (
+                <Food name={dish.name} picture={dish.image} />
+            ))}
         </div>
     );
 }
