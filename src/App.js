eafd8e718dd724810cbc588e00a7c38f50bcf509
yykdev <assavictory@gmail.com>
2019-08-31 23:54:54 +0900

    setState 시 기본 state 를 효율적으로 변경 할 수 있는 개선 안

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ class App extends React.Component {
     };
 
     add = () => {
-        this.setState({
-            count: this.state.count + 1
-        });
+        this.setState(current => ({
+            count: current.count + 1
+        }));
     };
     minus = () => {
-        this.setState({
-            count: this.state.count - 1
-        });
+        this.setState(current => ({
+            count: current.count - 1
+        }));
     };
 
     render() {
