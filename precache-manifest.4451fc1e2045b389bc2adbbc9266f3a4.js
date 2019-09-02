67df7e5eccfb3c4abedb7e0c04fce3b45f94e58c
yykdev <assavictory@gmail.com>
2019-09-02 13:54:50 +0900

    Updates

---
+++ precache-manifest.4451fc1e2045b389bc2adbbc9266f3a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f22ed8b83f08921a9ebc63ef6ba9bc",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "06302f11f3eda2dc45a1",
+    "url": "/movie_app/static/css/main.7706722b.chunk.css"
+  },
+  {
+    "revision": "e183bcee449302b2a05c",
+    "url": "/movie_app/static/js/2.6196da2f.chunk.js"
+  },
+  {
+    "revision": "06302f11f3eda2dc45a1",
+    "url": "/movie_app/static/js/main.6050b367.chunk.js"
+  },
+  {
+    "revision": "d179891a5f5e4ba41521",
+    "url": "/movie_app/static/js/runtime~main.76fa3daa.js"
+  }
+]);
